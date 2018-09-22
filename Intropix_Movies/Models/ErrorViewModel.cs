cc09a1e81a095aee43d996c9a019f23d5b0bc04e
TAMIR COHEN <tamircohen1@gmail.com>
2018-09-22 14:41:48 +0300

    Add project files.

---
+++ Intropix_Movies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Intropix_Movies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
