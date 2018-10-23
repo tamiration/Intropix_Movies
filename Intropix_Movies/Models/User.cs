10330a49499addb997a145a0d7fc56b4fd09bd17
TAMIR COHEN <tamircohen1@gmail.com>
2018-10-24 00:20:43 +0300

    updated the user model with migration to database

--- Intropix_Movies/Models/User.cs
+++ Intropix_Movies/Models/User.cs
@@ -9,5 +9,14 @@ namespace Intropix_Movies.Models
 {
     public class User
     {
+        [Key]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        [Display(Name = "Email")]
+        public string EMail { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public int Is_Manager { get; set; }
     }
 }
