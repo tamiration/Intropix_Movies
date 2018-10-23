85ce942cca88f28e121c7a5033a7cfa0bb93a827
TAMIR COHEN <tamircohen1@gmail.com>
2018-10-24 00:14:30 +0300

    add the import of System.ComponentModel.DataAnnotations;

--- Intropix_Movies/Models/Trailers.cs
+++ Intropix_Movies/Models/Trailers.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+
 
 namespace Intropix_Movies.Models
 {
---
+++ Intropix_Movies/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Intropix_Movies.Models
+{
+    public class User
+    {
+    }
+}
